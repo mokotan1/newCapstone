bb80267fbfeca81f5b5ab86af96a3c5c76c6b0b1
mokotan1 <103185768+mokotan1@users.noreply.github.com>
2026-06-08 15:11:22 +0900

    인트로 씬 오버레이 배경 추가

--- disputatio/Assets/godlotto/Script/Constants/SceneNames.cs
+++ disputatio/Assets/godlotto/Script/Constants/SceneNames.cs
@@ -1,6 +1,10 @@
 public static class SceneNames
 {
     public const string MainMenu = "MainMenuScene";
+    public const string IntroScene = "IntroScene";
+    public const string OpeningOffice = "Opening_Office";
+    public const string OpeningMention = "Opening_Mention";
+    public const string OpeningMentionOpen = "Opening_Mention _open";
     public const string MainScene = "MainScene";
     public const string Kitchen = "Kitchen";
     public const string MaidRoom = "MaidRoom";
